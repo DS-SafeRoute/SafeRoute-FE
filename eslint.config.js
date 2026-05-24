b9524a0187e8eba2c0019eccf70690ac7e4c38f4
urapp13ofmyeye <145529622+urapp13ofmyeye@users.noreply.github.com>
2026-05-25 02:00:43 +0900

    chore: Husky + lint-staged / MSW / Storybook 설정 (#4)
    * chore: Husky + lint-staged / MSW / Storybook 설정
    
    * style: Prettier 포맷 적용

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
@@ -5,25 +8,27 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
-    },
+export default tseslint.config({ ignores: ['dist'] }, {
+  extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
+  files: ['**/*.{ts,tsx}'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
   },
-);
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'prefer-template': 'error',
+    'eqeqeq': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+}, storybook.configs["flat/recommended"]);
---
+++ src/shared/mocks/handlers.ts
@@ -0,0 +1,7 @@
+export const handlers = [
+  // API 핸들러는 여기에 추가
+  // 예시:
+  // http.get('/api/buildings', () => {
+  //   return HttpResponse.json([{ id: 1, name: '본관' }]);
+  // }),
+];
