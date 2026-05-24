f7ac4f400a4f70b300c351289943c0bb71b9585e
박서현 <liz3035321@gmail.com>
2026-05-25 01:58:54 +0900

    style: Prettier 포맷 적용

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,5 @@
 import { Routes } from 'react-router-dom';
 
-const AppRoutes = () => (
-  <Routes>
-    {/* TODO: 페이지 추가 시 여기에 Route 작성 */}
-  </Routes>
-);
+const AppRoutes = () => <Routes>{/* TODO: 페이지 추가 시 여기에 Route 작성 */}</Routes>;
 
 export default AppRoutes;
