d9ff18fd9483983df2e62337c93ac4f9430a54bb
Soy <da5868@naver.com>
2026-05-30 01:56:25 +0900

    chore: 라우터 상수화

---
+++ src/shared/constants/path.ts
@@ -0,0 +1,10 @@
+export const ROUTES = {
+  HOME: '/',
+  SCENARIO_SETTINGS: '/scenarioSettings',
+  MANAGEMENT: '/management',
+  BUILDINGS: '/buildings',
+  FLOOR_PLANS: '/floorPlans',
+  CAMERAS: '/cameras',
+  TRAINING_ANALYSIS: '/trainingAnalysis',
+  REPORTS: '/reports',
+} as const;
