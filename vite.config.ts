36607476fcea9d0dcf22f33f7ff4b6ac586f0a85
Soy <da5868@naver.com>
2026-06-01 01:09:37 +0900

    fix: 누락 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import svgr from 'vite-plugin-svgr';
 import path from 'path';
+
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin(), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@apis': path.resolve(__dirname, './src/shared/apis'),
+      '@assets': path.resolve(__dirname, './src/shared/assets'),
+      '@components': path.resolve(__dirname, './src/shared/components'),
+      '@hooks': path.resolve(__dirname, './src/shared/hooks'),
+      '@styles': path.resolve(__dirname, './src/shared/styles'),
+      '@utils': path.resolve(__dirname, './src/shared/utils'),
+      '@constants': path.resolve(__dirname, './src/shared/constants'),
+      '@shared': path.resolve(__dirname, './src/shared'),
     },
   },
   server: {
