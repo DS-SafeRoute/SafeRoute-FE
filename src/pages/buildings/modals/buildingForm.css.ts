ed901d951cff676aae84532a0e8a11857ceacd71
Soy <da5868@naver.com>
2026-08-28 23:33:16 +0900

    Revert "fix: totalFloors 빌드 에러"
    This reverts commit 677749532a353fc186e8cad8f2cacaf299dfdc07.

--- src/pages/buildings/modals/buildingForm.css.ts
+++ src/pages/buildings/modals/buildingForm.css.ts
@@ -8,6 +8,11 @@ export const form = style({
   gap: vars.space.s4,
 });
 
+export const floorRow = style({
+  display: 'flex',
+  gap: vars.space.s4,
+});
+
 export const field = style({
   display: 'flex',
   flexDirection: 'column',
