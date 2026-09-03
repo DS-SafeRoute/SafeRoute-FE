d9ae7b7b508db19f2fce2173b96d32ec5c72a39f
soyyyyy <da5868@naver.com>
2026-09-03 23:12:56 +0900

    fix: 활성 IoT 유도등 개수 계산 수정

--- src/pages/scenarioSettings/hooks/useScenarioFloorView.ts
+++ src/pages/scenarioSettings/hooks/useScenarioFloorView.ts
@@ -248,8 +248,7 @@ export const useScenarioFloorView = ({
       id: 'iot',
       label: '활성 IoT 유도등',
       value: getMetricValue({
-        count: floorLightsQuery.data?.filter((light) => light.enabled && light.guidanceConfigured)
-          .length,
+        count: floorLightsQuery.data?.filter((light) => light.enabled).length,
         suffix: '개',
         isPending: metricPending,
         isUnavailable: !floorId || floorLightsQuery.isError,
